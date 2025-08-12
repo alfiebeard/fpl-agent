64973439af5dbc321b0ea73800a28654b7f3ba74
alfiebeard <alfiebeard96@gmail.com>
2025-08-12 22:45:54 +0100

    Removed redundant data imports

--- fpl_optimizer/ingestion/__init__.py
+++ fpl_optimizer/ingestion/__init__.py
@@ -3,14 +3,7 @@ Data ingestion package for FPL Optimizer
 """
 
 from .fetch_fpl import FPLDataFetcher
-from .fetch_understat import UnderstatDataFetcher
-from .fetch_fbref import FBRefDataFetcher
-from .test_data_fetcher import TestDataFetcher, get_test_data
 
 __all__ = [
-    "FPLDataFetcher",
-    "UnderstatDataFetcher", 
-    "FBRefDataFetcher",
-    "TestDataFetcher",
-    "get_test_data"
+    "FPLDataFetcher"
 ]
