1590001300c3b5015d29a2ec4be7d8ec2b1ebc70
alfiebeard <alfiebeard96@gmail.com>
2025-08-13 23:06:26 +0100

    Created player factory to centralise functions

--- fpl_agent/utils/__init__.py
+++ fpl_agent/utils/__init__.py
@@ -6,11 +6,13 @@ from .data_transformers import (
     transform_fpl_data_to_teams,
     TeamSummary
 )
+from .player_factory import PlayerFactory
 from .validator import FPLValidator, validate_llm_response
 
 __all__ = [
     "transform_fpl_data_to_teams",
     "TeamSummary",
     "FPLValidator",
-    "validate_llm_response"
+    "validate_llm_response",
+    "PlayerFactory"
 ] 
